589eaac3b4933a890c597246daf683ec32af6d43
Aykut Farsak <aykutfarsak@gmail.com>
2012-12-14 17:48:25 +0200

    nothing

--- src/index.php
+++ src/index.php
@@ -5,5 +5,6 @@ require 'User.php';
 $user = new User();
 echo $user->talk();
 
-echo '<br><br>Done!';
-
+echo '<br><br>';
+echo date('d.m.Y H:i', filemtime(__FILE__)).'<br>';
+echo date('d.m.Y H:i');
